70b0be6b5f849b1cbfb5b17f2d4054678875b858
Rohit <rrohitjee2018@gmail.com>
2021-10-18 15:13:32 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css';//change
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';//change
 
 
 
